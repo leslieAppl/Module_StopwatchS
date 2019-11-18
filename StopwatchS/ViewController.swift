c3209e274204f313d7e60b09fe6a2295517e7393
Leslie <leslie.appl@gmail.com>
2019-11-18 14:09:00 +0800

    init project

---
+++ StopwatchS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  StopwatchS
+//
+//  Created by leslie on 11/16/19.
+//  Copyright © 2019 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLbl: UILabel!
+    @IBOutlet weak var stopWatchBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func stopWatchBtnPressed(_ sender: Any) {
+        
+    }
+    
+}
+
